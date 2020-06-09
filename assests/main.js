f6b88d3be18634019fc5de9188b0c05103a0f8e0
Izuku <63758052+Beki-G@users.noreply.github.com>
2020-06-08 17:01:41 -0700

    add css style, add logic and api to display weather forecast

---
+++ assests/main.js
@@ -0,0 +1,98 @@
+function getUVIndex(lon, lat){
+    let queryURL = "http://api.openweathermap.org/data/2.5/uvi?appid=18bc71d7f6e0c92a913dd1a6fd41b1da&lat="+lat+"&lon="+lon;
+    
+    $.ajax({
+        url: queryURL,
+        method:"GET"
+    }).then(function(response){
+        let uvIndex = response.value
+        let currentUV = $("<div></div>").text("UV Index: "+ uvIndex);
+        $("#currentCard-stats").append(currentUV);
+    })
+
+}
+
+function displayCurrentWeather(currentData){
+    let currentTemp = $("<div></div>").text("Temperature: "+currentData.main.temp);
+    
+    let currentHumidity = $("<div></div>").text("Humidity: "+currentData.main.humidity);
+
+    let currentWind = $("<div></div>").text("Wind Speed: "+currentData.wind.speed)
+    
+    getUVIndex(currentData.coord.lon, currentData.coord.lat)
+
+    $("#currentCard-cityName").text(currentData.name)
+    $("#currentCard-stats").append(currentTemp);
+    $("#currentCard-stats").append(currentHumidity);
+    $("#currentCard-stats").append(currentWind);
+    
+}
+
+function formatDate(dateStr){
+    console.log(dateStr);
+    const d = new Date(dateStr)
+    const year = d.getFullYear();
+    const day = d.getDate();
+    const month = d.getMonth()+1;
+
+    let date = month+"/"+day+"/"+year;
+
+    return date
+}
+
+function displayForecastWeather(forecastData){
+    console.log("made it to DisplayForecast");
+    
+    let forecastArr = forecastData.list
+    let j = 0
+
+    for (let i =0; i<40; i+=8){
+        
+        let formattedDate = formatDate(forecastArr[i].dt_txt);
+
+        let forecastTemp = $("<div></div>").text("Temp: "+ forecastArr[i].main.temp)
+
+        let forecastHumidity =$("<div></div>").text("Humidity: "+ forecastArr[i].main.humidity);
+
+        let forecastChild = "#forcast-"+j;
+
+        $(forecastChild).text(formattedDate);
+        $(forecastChild).append(forecastTemp);
+        $(forecastChild).append(forecastHumidity);
+
+        j++
+    }
+
+}
+
+function getWeatherData(cityStr){
+    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+cityStr+"&appid=18bc71d7f6e0c92a913dd1a6fd41b1da&units=imperial"
+
+    //console.log(queryURL)
+    $.ajax({
+        url:queryURL,
+        method: "GET"        
+    }).then(function(response){
+        //console.log(response);
+        displayCurrentWeather(response);
+    })
+
+    let forecastQueryURL = "https://api.openweathermap.org/data/2.5/forecast?q="+cityStr+"&appid=18bc71d7f6e0c92a913dd1a6fd41b1da&units=imperial"
+
+    $.ajax({
+        url:forecastQueryURL,
+        method:"GET"
+    }).then(function(response){
+        console.log(response);
+        displayForecastWeather(response);
+    })
+
+}
+
+$("#citySearchbtn").click(function(event){
+    event.preventDefault();
+    let city = $("#cityName").val();
+
+    getWeatherData(city);
+
+})
